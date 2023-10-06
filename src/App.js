18a29db3f5283dd4e664c20f0dfc18bd5e2398ea
Deven Gangwani <devengangwani12@gmail.com>
2023-10-06 18:06:07 +0530

    Minor changes, got rid of irrelevant stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import RevoCalendar from "revo-calendar";
+import Login from "./LoginSignup/LoginSignup"
 
 function App() {
   return (
     <div className="App">
     	<h1> trivial Change </h1>
-      <RevoCalendar />
+      <Login />
     </div>
   );
 }
