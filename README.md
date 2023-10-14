2278627142e97b58e89d63eda69eef48af313471
Deven Gangwani <devengangwani12@gmail.com>
2023-10-14 22:58:00 +0530

    Added revo-calendar source

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+*Based on Revo-Calendar - https://github.com/gjmolter/revo-calendar
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import RevoCalendar from "revo-calendar";
+import React, {useState, useEffect, createContext} from "react";
+import RevoCalendar from "./revo-calendar";
+
+const UserContext = createContext({user:"user", organisation:""});
 
 function App() {
+	const [user, setUser] = useState({level: "user", organisation: ""});
+
   return (
     <div className="App">
-    	<h1> trivial Change </h1>
-      <RevoCalendar />
+    	<UserContext.Provider value={{
+    		user: user.level,
+    		organisation: user.organisation
+    	}}>
+			<h1> trivial Change </h1>
+			<RevoCalendar />
+		</UserContext.Provider>
     </div>
   );
 }
